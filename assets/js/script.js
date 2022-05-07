1406030c15673d44742e2af195093e5de2814615
rebahama <wiri@hotmail.se>
2022-05-07 11:42:01 +0000

    add api

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data.expiry);
+    } else{
+        throw new Error(data.error)
     }
+
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`
+    results += `<div class="key-status">${data}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 
 }
